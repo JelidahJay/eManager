7bfd8215a2073a798a648a92cd2695186fb6ff8b
JelidahJay <79125983+JelidahJay@users.noreply.github.com>
2022-04-13 14:03:35 +0200

    Add files via upload

---
+++ Departments.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace eManager
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        public string DeptName { get; set; } = String.Empty;
+
+        public string DeptCode { get; set; } = String.Empty;
+
+        public List<Users>? Users { get; set; }
+
+    }
+}
---
+++ Users.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eManager
+{
+
+ 
+    public class Users
+    {
+       [Key]
+        public int UserId { get; set; }
+
+        [ForeignKey("FK_Department")]
+        public int DepartmentId { get; set; }
+
+        public string FirstName { get; set; } = String.Empty;
+
+        public string LastName { get; set; } = String.Empty;
+
+        public string DOB { get; set; } = String.Empty;
+
+        public string Designation { get; set; } = String.Empty;
+
+        public string Email { get; set; } = String.Empty;
+
+        public string Cellphone { get; set; } = String.Empty;
+        public string ContactAddress { get; set; } = String.Empty;
+
+    }
+}
